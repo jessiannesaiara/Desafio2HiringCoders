c98f7e1625dc23e6beb8a9bff95b9b6e33a70618
Jessianne Saiara <jessiannesaiara.js@gmail.com>
2021-07-25 14:59:34 -0300

    ajuste na listagem

--- js/clientList.js
+++ js/clientList.js
@@ -2,20 +2,20 @@ let clientes = JSON.parse(localStorage.getItem("clientes"))
 
 let tbody = document.getElementById("tb-client")
 
-let lista  = clientes.map(function(cliente, indice) {
-  return `<tr>
-    <td>${cliente.nome}</td>
-    <td>${cliente.email}</td>
-    <td>${cliente.cpf}</td>
-    <td>${cliente.telefone}</td>
-    <td>${cliente.cep}</td>
-    <td>${cliente.endereco}</td>
-    <td>${cliente.bairro}</td>
-    <td>${cliente.cidade}</td>
-  </tr>`
-
-})
-
-tbody.innerHTML = lista.join('')
-
-
+if(clientes.length > 0) {
+  let lista  = clientes.map(function(cliente) {
+    return `<tr>
+      <td>${cliente.nome}</td>
+      <td>${cliente.email}</td>
+      <td>${cliente.cpf}</td>
+      <td>${cliente.telefone}</td>
+      <td>${cliente.cep}</td>
+      <td>${cliente.endereco}</td>
+      <td>${cliente.bairro}</td>
+      <td>${cliente.cidade}</td>
+    </tr>`
+  
+  })
+  
+  tbody.innerHTML = lista.join('')
+}
--- js/productList.js
+++ js/productList.js
@@ -2,17 +2,19 @@ let produtos = JSON.parse(localStorage.getItem("produtos"))
 
 let tbody = document.getElementById("tb-product")
 
-let lista  = produtos.map(function(produto, indice) {
-  return `<tr>
-    <td>${produto.codigo}</td>
-    <td>${produto.descricao}</td>
-    <td>${produto.fornecedor}</td>
-    <td>${produto.marca}</td>
-    <td>${produto.quantidade}</td>
-    <td>${produto.custo}</td>
-    <td>${produto.venda}</td>
-  </tr>`
-
-})
-
-tbody.innerHTML = lista.join('')
+if(produtos.length > 0) {
+  let lista  = produtos.map(function(produto) {
+    return `<tr>
+      <td>${produto.codigo}</td>
+      <td>${produto.descricao}</td>
+      <td>${produto.fornecedor}</td>
+      <td>${produto.marca}</td>
+      <td>${produto.quantidade}</td>
+      <td>${produto.custo}</td>
+      <td>${produto.venda}</td>
+    </tr>`
+  
+  })
+  
+  tbody.innerHTML = lista.join('')
+}
